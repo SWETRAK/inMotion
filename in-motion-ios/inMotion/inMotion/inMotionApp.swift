b8c4edb84ac463b125b08e62fb51a4abd5d2b4f0
kamilpietrak <kamilpietrak123@gmail.com>
2023-04-08 18:11:13 +0200

    Init commit

---
+++ in-motion-ios/inMotion/inMotion/inMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  inMotionApp.swift
+//  inMotion
+//
+//  Created by Kamil Pietrak on 08/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct inMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
