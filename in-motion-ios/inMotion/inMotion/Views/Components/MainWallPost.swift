d10bd44aa5bda8e5898855ccc32b325bb85a3b22
kamilpietrak <kamilpietrak123@gmail.com>
2023-06-06 13:24:09 +0200

    Working two screens

--- in-motion-ios/inMotion/inMotion/Views/Components/MainWallPost.swift
+++ in-motion-ios/inMotion/inMotion/Views/Components/MainWallPost.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct MainWallPost: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            HStack{
+                Image("google-logo")
+                    .resizable()
+                    .frame(width: 70, height: 70)
+                VStack{
+                    Text("Nickname")
+                    Text("Last seen 12")
+                }
+            }.frame(maxWidth: .infinity, alignment: .leading)
+            
+            GeometryReader {
+                proxy in
+                ZStack(alignment: .topTrailing) {
+                    Image("google-logo")
+                        .resizable()
+                        .frame(width: proxy.size.width, height: proxy.size.width)
+                    Image("facebook-logo")
+                        .resizable()
+                        .frame(width: proxy.size.width/2, height: proxy.size.width/2)
+                        .padding()
+                }
+            }
+            
+            HStack{
+                HStack{
+                    Text("👍")
+                    Text("12")
+                }
+                
+            }
+            
+            Divider()
+//                .foregroundColor(Color("accent"))
+            Spacer()
+            
+        }.padding()
     }
 }
 
--- in-motion-ios/inMotion/inMotion/Views/LoginView.swift
+++ in-motion-ios/inMotion/inMotion/Views/LoginView.swift
@@ -8,9 +8,59 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("logo")
+            
+            Spacer()
+            
+            VStack {
+                TextField("Email", text: $email)
+                
+                TextField("Password", text: $password)
+                
+                Button("Login with email") {
+                    
+                }
+                
+                Divider()
+                
+                HStack{
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image("google-logo")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                    }
+
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image("facebook-logo")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                    }
+                    Spacer()
+                }
+            }
+            
+            Spacer()
+            
+            Button("Register new account") {
+                
+            }
+            
+        }.padding()
     }
+    
 }
 
 struct LoginView_Previews: PreviewProvider {
