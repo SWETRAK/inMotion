925d7e94c6aeef141e77e7aea5978d63d6a8da93
kamilpietrak <kamilpietrak123@gmail.com>
2023-06-06 13:23:56 +0200

    Working one screen

---
+++ in-motion-ios/inMotion/inMotion/Views/Components/MainWallPost.swift
@@ -0,0 +1,20 @@
+//
+//  MainWallPost.swift
+//  inMotion
+//
+//  Created by Kamil Pietrak on 06/06/2023.
+//
+
+import SwiftUI
+
+struct MainWallPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainWallPost_Previews: PreviewProvider {
+    static var previews: some View {
+        MainWallPost()
+    }
+}
