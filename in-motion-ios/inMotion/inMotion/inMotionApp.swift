9c2a48ef4912220a95079dd096a5bf0896a1708c
kamilpietrak <kamilpietrak123@gmail.com>
2023-06-06 10:12:54 +0200

    changed project to implement core data

--- in-motion-ios/inMotion/inMotion/inMotionApp.swift
+++ in-motion-ios/inMotion/inMotion/inMotionApp.swift
@@ -2,16 +2,19 @@
 //  inMotionApp.swift
 //  inMotion
 //
-//  Created by Kamil Pietrak on 08/04/2023.
+//  Created by Kamil Pietrak on 06/06/2023.
 //
 
 import SwiftUI
 
 @main
 struct inMotionApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
