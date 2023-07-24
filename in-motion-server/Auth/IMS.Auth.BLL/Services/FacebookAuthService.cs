fdc58741904da7e56a2ba08b7ae5500381aeebb9
Swetrak <kamilpietrak123@gmail.com>
2023-07-24 16:58:36 +0200

    Almost working facebook login

--- in-motion-server/Auth/IMS.Auth.BLL/Services/FacebookAuthService.cs
+++ in-motion-server/Auth/IMS.Auth.BLL/Services/FacebookAuthService.cs
@@ -1,7 +1,6 @@
 using System.Net.Http.Headers;
 using System.Net.Mime;
 using AutoMapper;
-using IMS.Auth.BLL.Authentication;
 using IMS.Auth.Domain.Consts;
 using IMS.Auth.Domain.Entities;
 using IMS.Auth.IBLL.Services;
@@ -9,6 +8,7 @@ using IMS.Auth.IDAL.Repositories;
 using IMS.Auth.Models.Dto.Incoming;
 using IMS.Auth.Models.Dto.Outgoing;
 using IMS.Auth.Models.Exceptions;
+using IMS.Auth.Models.Models;
 using IMS.Shared.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
@@ -50,22 +50,8 @@ public class FacebookAuthService : IFacebookAuthService
     {
         var requestTime = DateTime.UtcNow;
         var facebookResponseData = await GetFacebookUserAsync(authenticateWithFacebookProviderDto.Token);
-        
-        Console.WriteLine(JsonConvert.SerializeObject(facebookResponseData));
-        // User user;
-        // var provider = await _providerRepository.GetByTokenWithUserAsync(
-        //     Providers.Facebook, authenticateWithFacebookProviderDto.FacebookToken);
-        // if (provider is null)
-        // {
-        //     //user = await CheckIfEmailTaken(payload, authenticateWithGoogleProviderDto.UserId);
-        //     user = new User();
-        // }
-        // else
-        // {
-        //     user = CheckIfActiveUser(provider);
-        // }
-        
-        var responseData = _mapper.Map<UserInfoDto>(new User());
+        var user = await CheckProvider(authenticateWithFacebookProviderDto, facebookResponseData);
+        var responseData = _mapper.Map<UserInfoDto>(user);
         
         return new ImsHttpMessage<UserInfoDto>
         {
@@ -82,27 +68,78 @@ public class FacebookAuthService : IFacebookAuthService
     /// <param name="accessToken">Token from frontend application</param>
     /// <returns>Dictionary with email and user id field</returns>
     /// <exception cref="Exception"></exception>
-    private async Task<Dictionary<string, string>> GetFacebookUserAsync(string accessToken)
+    private async Task<FacebookUserResource> GetFacebookUserAsync(string accessToken)
     {
-        var response = await _httpClient.GetAsync($"me?access_token={accessToken}&fields=email");
+        var response = await _httpClient.GetAsync($"me?access_token={accessToken}&fields=email,first_name,last_name");
 
         if (!response.IsSuccessStatusCode)
             throw new Exception("User from this token not exist");
         
         var responseBodyString = await response.Content.ReadAsStringAsync();
-        
         var data = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseBodyString);
-        return data;
+        
+        //TODO: Check names of keys here
+        Console.WriteLine(responseBodyString);
+        return new FacebookUserResource
+        {
+            Email = data["email"],
+            FirstName = data["first_name"],
+            LastName = data["last_name"],
+            UserId = data["id"]
+        };
+    }
+
+    private async Task<User> CheckProvider(AuthenticateWithFacebookProviderDto authenticateWithFacebookProviderDto, FacebookUserResource facebookAuthResponse)
+    {
+        var provider = await _providerRepository.GetByTokenWithUserAsync(
+            Providers.Facebook, authenticateWithFacebookProviderDto.Token);
+        
+        if (provider is null)
+        {
+            return await CheckIfEmailTaken(facebookAuthResponse, authenticateWithFacebookProviderDto.UserId);
+        }
+        
+        return CheckIfActiveUser(provider);
     }
     
-    private async Task<User> CheckIfEmailTaken(string email, dynamic payload, string providerKey)
+    private async Task<User> CheckIfEmailTaken(FacebookUserResource facebookPayload, string providerKey)
     {
-        var tempUser = await _userRepository.GetByEmailAsync(email);
+        var tempUser = await _userRepository.GetByEmailAsync(facebookPayload.Email);
         if (tempUser is not null)
-            throw new UserWithEmailAlreadyExistsException(email);
+            throw new UserWithEmailAlreadyExistsException(facebookPayload.Email);
         
-        // return await CreateNewUserFromPayload(payload, providerKey);
-        return new User();
+        return await CreateNewUserFromPayload(facebookPayload, providerKey);
+    }
+    
+    private async Task<User> CreateNewUserFromPayload(FacebookUserResource payload, string providerKey)
+    {
+        var activationCode = Guid.NewGuid().ToString();
+
+        var newProvider = CreateNewProvider(providerKey);
+        
+        var user = new User
+        {
+            Email = payload.Email,
+            Nickname = $"{payload.FirstName} {payload.LastName}",
+            ConfirmedAccount = false,
+            ActivationToken = activationCode,
+            Role = Roles.User ,
+            Providers = new List<Provider> {newProvider}
+        };
+        
+        await _userRepository.Save();
+        
+        return user;
+    }
+    
+    private Provider CreateNewProvider(string providerKey)
+    {
+        Console.WriteLine(providerKey);
+        return new Provider
+        {
+            AuthKey = providerKey,
+            Name = Providers.Facebook,
+        };
     }
     
     private static User CheckIfActiveUser(Provider provider)
--- in-motion-server/Auth/IMS.Auth.Models/Models/FacebookUserResource.cs
+++ in-motion-server/Auth/IMS.Auth.Models/Models/FacebookUserResource.cs
@@ -2,5 +2,8 @@ namespace IMS.Auth.Models.Models;
 
 public class FacebookUserResource
 {
-    
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserId { get; set; }
 }
