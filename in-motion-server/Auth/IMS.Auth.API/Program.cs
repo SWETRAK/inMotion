2bf97f1c85d359091eeac771852b6876d25d48fd
Swetrak <kamilpietrak123@gmail.com>
2023-08-26 14:45:09 +0200

    SwaggerUI added to backend services

--- in-motion-server/Auth/IMS.Auth.API/Program.cs
+++ in-motion-server/Auth/IMS.Auth.API/Program.cs
@@ -35,14 +35,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    
     // Auto migrations are enabled in development mode
     using var scope = app.Services.CreateScope();
     var dbContext = scope.ServiceProvider.GetRequiredService<ImsAuthDbContext>();
     dbContext.Database.Migrate();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 
