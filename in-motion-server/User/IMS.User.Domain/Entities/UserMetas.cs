7544e56c1d4ebb50016348de1d892a02e7b3531d
Swetrak <kamilpietrak123@gmail.com>
2023-08-30 17:05:15 +0200

    Added init migration for user

--- in-motion-server/User/IMS.User.Domain/Entities/UserMetas.cs
+++ in-motion-server/User/IMS.User.Domain/Entities/UserMetas.cs
@@ -7,8 +7,6 @@ public class UserMetas
     public Guid UserExternalId { get; set; }
 
     public string Bio { get; set; }
-    
-    // This should be nullable because of null user profile video in registration 
     public Guid? ProfileVideoId { get; set; }
     public virtual UserProfileVideo ProfileVideo { get; set; }
 }
