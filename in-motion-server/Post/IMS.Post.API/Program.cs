22dba7f9ebfc39ece4fffd8aa4e5ffbe672ebbe8
SWETRAK <kamilpietrak123@icloud.com>
2023-09-10 14:57:39 +0200

    Post service fix start

--- in-motion-server/Post/IMS.Post.API/Program.cs
+++ in-motion-server/Post/IMS.Post.API/Program.cs
@@ -1,5 +1,6 @@
 using IMS.Post.BLL;
 using IMS.Post.DAL;
+using IMS.Post.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddPostValidators();
 builder.Services.AddPostServices();
 
 builder.Services.AddControllers();
+builder.Services.AddPostMappers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
