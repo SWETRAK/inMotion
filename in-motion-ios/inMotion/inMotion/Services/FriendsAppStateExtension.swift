08544ae8ef69de8ebb1076cf148f734224c79c8a
SWETRAK <kamilpietrak123@icloud.com>
2023-11-08 16:18:59 +0100

    [A][IOS-116] Added friendship call methods to background services

--- in-motion-ios/inMotion/inMotion/Services/FriendsAppStateExtension.swift
+++ in-motion-ios/inMotion/inMotion/Services/FriendsAppStateExtension.swift
@@ -10,11 +10,326 @@ import Foundation
 // MARK: - Friends Methods
 extension AppState {
     
-    func 
+    func createFriendshipHttpRequest(otherUserId: UUID,
+                                 successCreateFriendshipAction: @escaping (InvitationFriendshipDto) -> Void,
+                                 failureCreateFriendshipAction: @escaping (ImsHttpError) -> Void) {
+        
+        var request = URLRequest(url: URL(string: "\(self.httpBaseUrl)/friends/api/friends/\(otherUserId.uuidString)")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer \(self.token!)", forHTTPHeaderField: "Authorization")
+        
+        request.httpMethod = HTTPMethods.POST.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                if let error = error as? NSError {
+                    failureCreateFriendshipAction(ImsHttpError(status: 500,  errorMessage: error.localizedDescription, errorType: ""))
+                }
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 201)
+                {
+                    if let safeImsMessage: ImsHttpMessage<InvitationFriendshipDto> = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpMessage<InvitationFriendshipDto>.self) {
+                        print(safeImsMessage.status)
+                        if let userInfoDataSafe: InvitationFriendshipDto = safeImsMessage.data {
+                            successCreateFriendshipAction(userInfoDataSafe);
+                        }
+                    }
+                } else {
+                    if var safeError: ImsHttpError = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpError.self) {
+                        if (httpResponse.statusCode == 500)
+                        {
+                            safeError.status = 500
+                        }
+                        failureCreateFriendshipAction(safeError);
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
     
+    func acceptFriendshipHttpRequest(friendshipId: UUID,
+                                 successAcceptUserAction: @escaping (AcceptedFriendshipDto) -> Void,
+                                 failureAcceptUserAction: @escaping (ImsHttpError) -> Void) {
+        var request = URLRequest(url: URL(string: "\(self.httpBaseUrl)/friends/api/friends/accept/\(friendshipId.uuidString)")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer \(self.token!)", forHTTPHeaderField: "Authorization")
+        
+        request.httpMethod = HTTPMethods.PUT.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                if let error = error as? NSError {
+                    failureAcceptUserAction(ImsHttpError(status: 500,  errorMessage: error.localizedDescription, errorType: ""))
+                }
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 200)
+                {
+                    if let safeImsMessage: ImsHttpMessage<AcceptedFriendshipDto> = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpMessage<AcceptedFriendshipDto>.self) {
+                        print(safeImsMessage.status)
+                        if let userInfoDataSafe: AcceptedFriendshipDto = safeImsMessage.data {
+                            successAcceptUserAction(userInfoDataSafe);
+                        }
+                    }
+                } else {
+                    if var safeError: ImsHttpError = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpError.self) {
+                        if (httpResponse.statusCode == 500)
+                        {
+                            safeError.status = 500
+                        }
+                        failureAcceptUserAction(safeError);
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func rejectFriendshipHttpRequest(friendshipId: UUID,
+                                 successRejectFriendshipAction: @escaping (RejectedFriendshipDto) -> Void,
+                                 failureRejectFriendshipAction: @escaping (ImsHttpError) -> Void) {
+        
+        var request = URLRequest(url: URL(string: "\(self.httpBaseUrl)/friends/api/friends/reject/\(friendshipId.uuidString)")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer \(self.token!)", forHTTPHeaderField: "Authorization")
+        
+        request.httpMethod = HTTPMethods.PATCH.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                if let error = error as? NSError {
+                    failureRejectFriendshipAction(ImsHttpError(status: 500,  errorMessage: error.localizedDescription, errorType: ""))
+                }
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 200)
+                {
+                    if let safeImsMessage: ImsHttpMessage<RejectedFriendshipDto> = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpMessage<RejectedFriendshipDto>.self) {
+                        print(safeImsMessage.status)
+                        if let userInfoDataSafe: RejectedFriendshipDto = safeImsMessage.data {
+                            successRejectFriendshipAction(userInfoDataSafe);
+                        }
+                    }
+                } else {
+                    if var safeError: ImsHttpError = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpError.self) {
+                        if (httpResponse.statusCode == 500)
+                        {
+                            safeError.status = 500
+                        }
+                        failureRejectFriendshipAction(safeError);
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func revertFriendshipHttpRequest(friendshipId: UUID,
+                                 successRevertFriendshipAction: @escaping (Bool) -> Void,
+                                 failureRevertFriendshipAction: @escaping (ImsHttpError) -> Void) {
+        
+        var request = URLRequest(url: URL(string: "\(self.httpBaseUrl)/friends/api/friends/revert/\(friendshipId.uuidString)")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer \(self.token!)", forHTTPHeaderField: "Authorization")
+
+        request.httpMethod = HTTPMethods.DELETE.rawValue
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+              if let error = error as? NSError {
+                  failureRevertFriendshipAction(ImsHttpError(status: 500,  errorMessage: error.localizedDescription, errorType: ""))
+              }
+            return
+          }
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 200)
+                {
+                    if let safeImsMessage: ImsHttpMessage<Bool> = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpMessage<Bool>.self) {
+                        print(safeImsMessage.status)
+                        if let userInfoDataSafe: Bool = safeImsMessage.data {
+                            successRevertFriendshipAction(userInfoDataSafe);
+                        }
+                    }
+                } else {
+                    if var safeError: ImsHttpError = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpError.self) {
+                        if (httpResponse.statusCode == 500)
+                        {
+                            safeError.status = 500
+                        }
+                        failureRevertFriendshipAction(safeError);
+                    }
+                }
+            }
+        }
+
+        task.resume()
+    }
+    
+    func unfiendsFriendshipHttpRequest(friendshipId: UUID,
+                                 successUnfriendFriendshipAction: @escaping (RejectedFriendshipDto) -> Void,
+                                 failureUnfriendFriendshipAction: @escaping (ImsHttpError) -> Void) {
+        
+        var request = URLRequest(url: URL(string: "\(self.httpBaseUrl)/friends/api/friends/unfriend/\(friendshipId.uuidString)")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer \(self.token!)", forHTTPHeaderField: "Authorization")
+
+        request.httpMethod = HTTPMethods.DELETE.rawValue
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+              if let error = error as? NSError {
+                  failureUnfriendFriendshipAction(ImsHttpError(status: 500,  errorMessage: error.localizedDescription, errorType: ""))
+              }
+            return
+          }
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 200)
+                {
+                    if let safeImsMessage: ImsHttpMessage<RejectedFriendshipDto> = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpMessage<RejectedFriendshipDto>.self) {
+                        print(safeImsMessage.status)
+                        if let userInfoDataSafe: RejectedFriendshipDto = safeImsMessage.data {
+                            successUnfriendFriendshipAction(userInfoDataSafe);
+                        }
+                    }
+                } else {
+                    if var safeError: ImsHttpError = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpError.self) {
+                        if (httpResponse.statusCode == 500)
+                        {
+                            safeError.status = 500
+                        }
+                        failureUnfriendFriendshipAction(safeError);
+                    }
+                }
+            }
+        }
+
+        task.resume()
+    }
 }
 
 // MARK: - FriendsList Methods
 extension AppState {
     
+    func getListOfAcceptedFriendshipHttpRequest(successGetRelations: @escaping ([AcceptedFriendshipDto]) -> Void,
+                                     failureGetRelations: @escaping (ImsHttpError) -> Void) {
+        
+        var request = URLRequest(url: URL(string: "\(self.httpBaseUrl)/friends/api/friends/lists/accepted")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer \(self.token!)", forHTTPHeaderField: "Authorization")
+        
+        request.httpMethod = HTTPMethods.GET.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                if let error = error as? NSError {
+                    failureGetRelations(ImsHttpError(status: 500,  errorMessage: error.localizedDescription, errorType: ""))
+                }
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 200)
+                {
+                    if let safeImsMessage: ImsHttpMessage<Array<AcceptedFriendshipDto>> = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpMessage<Array<AcceptedFriendshipDto>>.self) {
+                        print(safeImsMessage.status)
+                        if let userInfoDataSafe: Array<AcceptedFriendshipDto> = safeImsMessage.data {
+                            successGetRelations(userInfoDataSafe);
+                        }
+                    }
+                } else {
+                    if var safeError: ImsHttpError = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpError.self) {
+                        // INFO: This throws 404 if user has no friends
+                        if (httpResponse.statusCode == 500)
+                        {
+                            safeError.status = 500
+                        }
+                        failureGetRelations(safeError);
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func getListOfRequestedFriendshipHttpRequest(successGetRelations: @escaping ([RequestFriendshipDto]) -> Void,
+                                      failureGetRelations: @escaping (ImsHttpError) -> Void) {
+        
+        var request = URLRequest(url: URL(string: "\(self.httpBaseUrl)/friends/api/friends/lists/requested")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer \(self.token!)", forHTTPHeaderField: "Authorization")
+        
+        request.httpMethod = HTTPMethods.GET.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                if let error = error as? NSError {
+                    failureGetRelations(ImsHttpError(status: 500,  errorMessage: error.localizedDescription, errorType: ""))
+                }
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 200)
+                {
+                    if let safeImsMessage: ImsHttpMessage<Array<RequestFriendshipDto>> = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpMessage<Array<RequestFriendshipDto>>.self) {
+                        print(safeImsMessage.status)
+                        if let userInfoDataSafe: Array<RequestFriendshipDto> = safeImsMessage.data {
+                            successGetRelations(userInfoDataSafe);
+                        }
+                    }
+                } else {
+                    if var safeError: ImsHttpError = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpError.self) {
+                        if (httpResponse.statusCode == 500)
+                        {
+                            safeError.status = 500
+                        }
+                        failureGetRelations(safeError);
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func getListOfInvitedFriendshipHttpRequest(successGetRelations: @escaping ([InvitationFriendshipDto]) -> Void,
+                                    failureGetRelations: @escaping (ImsHttpError) -> Void) {
+        
+        var request = URLRequest(url: URL(string: "\(self.httpBaseUrl)/friends/api/friends/lists/invited")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer \(self.token!)", forHTTPHeaderField: "Authorization")
+        
+        request.httpMethod = HTTPMethods.GET.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                if let error = error as? NSError {
+                    failureGetRelations(ImsHttpError(status: 500,  errorMessage: error.localizedDescription, errorType: ""))
+                }
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 200)
+                {
+                    if let safeImsMessage: ImsHttpMessage<Array<InvitationFriendshipDto>> = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpMessage<Array<InvitationFriendshipDto>>.self) {
+                        print(safeImsMessage.status)
+                        if let userInfoDataSafe: Array<InvitationFriendshipDto> = safeImsMessage.data {
+                            successGetRelations(userInfoDataSafe);
+                        }
+                    }
+                } else {
+                    if var safeError: ImsHttpError = JsonUtil.decodeJsonData(data: data, returnModelType: ImsHttpError.self) {
+                        if (httpResponse.statusCode == 500)
+                        {
+                            safeError.status = 500
+                        }
+                        failureGetRelations(safeError);
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
 }
