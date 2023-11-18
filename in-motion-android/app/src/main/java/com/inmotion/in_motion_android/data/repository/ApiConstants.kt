4a5c4eab16ceac17b7ec416569b0ccaffb245a2b
s95520 <s95520@pollub.edu.pl>
2023-11-18 13:12:55 +0100

    [Develop] changed paths for profile videos, modified authorization header name, get profile video as mp4 and gif doesn't require nickname, post now receives postId, rather than generating it.

--- in-motion-android/app/src/main/java/com/inmotion/in_motion_android/data/repository/ApiConstants.kt
+++ in-motion-android/app/src/main/java/com/inmotion/in_motion_android/data/repository/ApiConstants.kt
@@ -1,5 +1,5 @@
 package com.inmotion.in_motion_android.data.repository
 
 object ApiConstants {
-    val BASE_URL = "http://10.0.2.2:8081"
+    val BASE_URL = "http://192.168.0.67:8081"
 }
--- in-motion-android/app/src/main/java/com/inmotion/in_motion_android/fragment/MainFragment.kt
+++ in-motion-android/app/src/main/java/com/inmotion/in_motion_android/fragment/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.inmotion.in_motion_android.fragment
 
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,26 +12,34 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.inmotion.in_motion_android.R
 import com.inmotion.in_motion_android.adapter.PostsAdapter
 import com.inmotion.in_motion_android.data.PostDto
-import com.inmotion.in_motion_android.data.UserDto
+import com.inmotion.in_motion_android.data.dto.user.FullUserInfoDto
+import com.inmotion.in_motion_android.data.repository.RepositoryCallback
+import com.inmotion.in_motion_android.data.repository.UserRepository
 import com.inmotion.in_motion_android.databinding.FragmentMainBinding
 import com.inmotion.in_motion_android.util.FocusedItemFinder
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
+    private lateinit var user: FullUserInfoDto
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            user = arguments?.getSerializable("USER", FullUserInfoDto::class.java)!!
+        }
+
         binding = FragmentMainBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
         binding.mainFragmentToolbar.setLogo(R.drawable.ic_in_motion_logo)
+
 
         val adapter = PostsAdapter(getPosts())
         binding.rvPosts.adapter = adapter
@@ -53,7 +62,7 @@ class MainFragment : Fragment() {
             val bundle = Bundle()
             bundle.putSerializable(
                 "USER",
-                UserDto("CoolNickname", "Very long interesting user bio.")
+                user
             )
             findNavController()
                 .navigate(R.id.action_mainFragment_to_userDetailsFragment, bundle)
